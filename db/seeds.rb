9f9247ba0a814f3475f680c14cf2108baa4790fb
artech <https://github.com/nirajartech/hello.git>
2018-12-08 12:34:58 +0530

    removeadmin,signout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Creating roles"
-['librarian','student','admin'].each do |role|
+['librarian','student'].each do |role|
   Role.find_or_create_by(name: role)
 end
